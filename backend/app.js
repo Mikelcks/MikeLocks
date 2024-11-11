83dcea2af73151c8764489298d41f0511b3c0274
Mike Locks <mikeloc@hotmail.fr>
2024-11-11 15:36:23 +0100

    changement config nodemailer

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,9 @@ app.post('/send-email', async (req, res) => {
 
     // Configuration de Nodemailer
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true pour le port 465, false pour les autres ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
