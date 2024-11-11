f539ac356fd5d6f40da0db9612044cd3a43bec53
Mike Locks <mikeloc@hotmail.fr>
2024-11-11 21:27:51 +0100

    suppression lazy

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,14 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Bio from './pages/Bio';
+import Contact from './pages/Contact';
 import './utils/style/global.scss';
-
-// Utilisation de React.lazy pour charger les pages uniquement quand l'utilisateur navigue vers elles
-const Home = lazy(() => import('./pages/Home'));
-const Services = lazy(() => import('./pages/Services'));
-const Portfolio = lazy(() => import('./pages/Portfolio'));
-const Bio = lazy(() => import('./pages/Bio'));
-const Contact = lazy(() => import('./pages/Contact'));
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -19,15 +17,13 @@ root.render(
     <React.StrictMode>
         <Router>
             <Header />
-            <Suspense fallback={<div>Chargement...</div>}>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
-                    <Route path="/bio" element={<Bio />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </Suspense>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/bio" element={<Bio />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
             <Footer />
         </Router>
     </React.StrictMode>
