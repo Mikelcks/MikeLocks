d2cdb96f07dcdaa9218f85d3d88a7e7a8b6ed1c6
Mike Locks <mikeloc@hotmail.fr>
2024-11-11 16:53:22 +0100

    changement port + delete secure

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,7 @@ app.post('/send-email', async (req, res) => {
     // Configuration de Nodemailer
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true pour le port 465, false pour les autres ports
+        port: 465,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
